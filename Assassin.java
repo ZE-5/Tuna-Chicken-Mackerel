b3552b57ad2531a6eefee3f0a689624255fb588f
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-13 16:43:12 -0400

    Stalking behaviour

--- Assassin.java
+++ Assassin.java
@@ -6,12 +6,12 @@ public class Assassin extends Enemy {
     private int t, atk_t;
     private int projectileY, projectileX;
     private EnemyProjectile projectile;
-    private final int CHARGE = 100, ATTACK_DUR = 20;
+    private final int THROW_CHARGE = 100, CLOSE_CHARGE = 50, ATTACK_DUR = 20;
     public Assassin(Player player, int x, int y) {
-        super(player, x, y, 40, 60, 100, 20, 2, 2, 40);
+        super(player, x, y, 40, 60, 100, 50, 2, 2, 40);
         t = 0;
         atk_t = -1;
-        projectile = new EnemyProjectile(x, y, 10, 10, 10, 5, 50);
+        projectile = new EnemyProjectile(x, y, 10, 10, 1, 5, 50);
     }
     
     public void update() {
@@ -21,7 +21,28 @@ public class Assassin extends Enemy {
         else
             projectileX = x;
         stopAttack();
-        if (!inRange()) {
+
+        if (!playerFacingMe()) {
+            moveToPlayer();
+            if (inCloseRange()) {
+                t++;
+                if (t == CLOSE_CHARGE) {
+                    t = 0;
+                    atk_t = 0;
+                    attack();
+                }
+
+            }
+            if (atk_t >= 0) {
+                if (atk_t == ATTACK_DUR) {
+                    atk_t = -2;
+                }
+                atk_t++;
+            }
+            return;
+        }
+
+        if (!inLongRange()) {
             t = 0;
             atk_t = -1;
             stopAttack();
@@ -29,21 +50,17 @@ public class Assassin extends Enemy {
         }
         else {
             t++;
-            if (t == CHARGE) {
+            if (t == THROW_CHARGE) {
                 t = 0;
-                atk_t = 0;
                 if (!projectile.isActive()) {
                     projectile.fire(projectileX, projectileY, isFacingRight);
-                }
-                    
+                }      
             }
         }
-        if (atk_t >= 0) {
-            if (atk_t == ATTACK_DUR) {
-                atk_t = -2;
-            }
-            atk_t++;
-        }
+    }
+
+    private boolean playerFacingMe() {
+        return (player.isFacingRight() && x > player.getX()) || (!player.isFacingRight() && x <= player.getX());
     }
 
     public void draw(Graphics2D g2) {
@@ -54,7 +71,11 @@ public class Assassin extends Enemy {
         g2.fillRect(x, y, width, height);
     }
 
-    private boolean inRange() {
+    private boolean inCloseRange() {
+        return getBoundingBox().intersects(player.getBoundingBox());
+    }
+
+    private boolean inLongRange() {
         Rectangle2D range = new Rectangle2D.Double(0, 0, 0, 0);
         if (isFacingRight) {
             range = new Rectangle2D.Double(getX() + width, getY(), width * 10, height);
