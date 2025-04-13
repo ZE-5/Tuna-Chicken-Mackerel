9afd3c505b4f6fd190990075c7ea693114e0dd69
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-13 11:19:46 -0400

    Abstract "projectile line" maths in EnemyProjectile

--- EnemyProjectile.java
+++ EnemyProjectile.java
@@ -1,5 +1,9 @@
 public final class EnemyProjectile extends Projectile{
     public EnemyProjectile(int x1, int y1, int x2, int y2, int c, int width, int height, int damage, int speed, int timeOut) {
         super(x1, y1, x2, y2, c, width, height, damage, speed, timeOut);
-    }    
+    }
+    
+    public EnemyProjectile(int x, int y, int width, int height, int damage, int speed, int timeOut) {
+        super(x, y, x + 1, y, y, width, height, damage, speed, timeOut);
+    }
 }
