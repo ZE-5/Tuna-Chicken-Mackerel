0379440a768844e76cf2da017956102bd5a5fd5d
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-13 18:25:57 -0400

    Behaviour as a state machine

--- Assassin.java
+++ Assassin.java
@@ -6,14 +6,27 @@ public class Assassin extends Enemy {
     private int t, atk_t;
     private int projectileY, projectileX;
     private EnemyProjectile projectile;
-    private final int THROW_CHARGE = 100, CLOSE_CHARGE = 50, ATTACK_DUR = 20;
+    private final int THROW_CHARGE = 100, CLOSE_CHARGE = 50, ATTACK_DUR = 20, RETREAT_DUR = 200;
+
+    private enum State {
+        STALK,
+        SHANK,
+        SCARED,
+        THROW
+    }
+
+    private State state;
+    private AssassinBehaviour behaviour;
+
     public Assassin(Player player, int x, int y) {
-        super(player, x, y, 40, 60, 100, 50, 2, 2, 40);
+        super(player, x, y, 40, 60, 100, 20, 2, 2, 40);
         t = 0;
         atk_t = -1;
+        state = State.STALK;
+        behaviour = new AssassinBehaviour();
         projectile = new EnemyProjectile(x, y, 10, 10, 1, 5, 50);
     }
-    
+
     public void update() {
         projectileY = y;
         if (isFacingRight)
@@ -21,46 +34,33 @@ public class Assassin extends Enemy {
         else
             projectileX = x;
         stopAttack();
-
+        System.out.println(t);
         if (!playerFacingMe()) {
-            moveToPlayer();
+            state = State.STALK;
+            if (inCloseRange())
+                state = State.SHANK;
+        } else {
             if (inCloseRange()) {
-                t++;
-                if (t == CLOSE_CHARGE) {
-                    t = 0;
-                    atk_t = 0;
-                    attack();
+                state = State.SCARED;
+            } else {
+                if (inLongRange()) {
+                    state = State.THROW;
                 }
-
-            }
-            if (atk_t >= 0) {
-                if (atk_t == ATTACK_DUR) {
-                    atk_t = -2;
-                }
-                atk_t++;
-            }
-            return;
-        }
-
-        if (!inLongRange()) {
-            t = 0;
-            atk_t = -1;
-            stopAttack();
-            moveToPlayer();
-        }
-        else {
-            t++;
-            if (t == THROW_CHARGE) {
-                t = 0;
-                if (!projectile.isActive()) {
-                    projectile.fire(projectileX, projectileY, isFacingRight);
-                }      
             }
         }
+        behaviour.act(state);
     }
 
     private boolean playerFacingMe() {
         return (player.isFacingRight() && x > player.getX()) || (!player.isFacingRight() && x <= player.getX());
+    }
+
+    private void retreat() {
+        if (player.isFacingRight()) {
+            move("RIGHT");
+        } else {
+            move("LEFT");
+        }
     }
 
     public void draw(Graphics2D g2) {
@@ -88,5 +88,50 @@ public class Assassin extends Enemy {
     protected Rectangle2D generateAttackBoundingBox() {
         return getBoundingBox();
     }
-    
+
+    private class AssassinBehaviour {
+        private void act(State current) {
+            switch (current) {
+                case STALK:
+                    t = 0;
+                    atk_t = -1;
+                    moveToPlayer();
+                    break;
+                case SHANK:
+                    t++;
+                    if (t == CLOSE_CHARGE) {
+                        t = 0;
+                        atk_t = 0;
+                        attack();
+                    }
+                    if (atk_t >= 0) {
+                        if (atk_t == ATTACK_DUR) {
+                            atk_t = -2;
+                        }
+                        atk_t++;
+                    }
+                    break;
+                case SCARED:
+                    retreat();
+                    t++;
+                    if (t == RETREAT_DUR) {
+                        t = 0;
+                        state = State.STALK;
+                        System.out.println("HERE");
+                    }
+                    break;
+                case THROW:
+                    t++;
+                    if (t == THROW_CHARGE) {
+                        t = 0;
+                        if (!projectile.isActive()) {
+                            projectile.fire(projectileX, projectileY, isFacingRight);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
