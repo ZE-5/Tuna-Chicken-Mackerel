2a2af2c2862ef09fad94ce1ed5f7282e455b75a8
ZE-5-M <zaneiedwards2003@gmail.com>
2025-04-17 10:40:11 -0400

    Added Treadmill that only works on player. Can customize direction (as a string that automatically formats itself) and move rate. You can also update the move rate if desired.

--- CollisionManager.java
+++ CollisionManager.java
@@ -12,10 +12,11 @@ public class CollisionManager {
     }
 
     public void checkCollisions(boolean[] keys) {
+        // Retrieve player information
         Rectangle2D playerBoundingBox = player.getBoundingBox();
         Rectangle2D playerAttackBoundingBox = player.getAttackBoundingBox();
 
-
+        //Sort out projectiles
         Vector<Projectile> projectiles = new Vector<Projectile>();
         for (GameEntity entity : gameEntities) {
             if (!(entity instanceof Projectile))
@@ -31,6 +32,7 @@ public class CollisionManager {
         }
 
 
+        //Loop through game entities
         Iterator<Projectile> projectileIterator;
         Iterator<GameEntity> entityIterator = gameEntities.iterator();
         while (entityIterator.hasNext()) {
@@ -60,9 +62,11 @@ public class CollisionManager {
                         projectileIterator.remove();
                     }
                 }
+                continue;
             }
 
-            else if (entity instanceof Wall) { //Handling walls
+            //Handling walls (player and projectile collisions)
+            else if (entity instanceof Wall) { 
                 Wall wall = (Wall) entity;
 
                 //Wall collision
@@ -90,19 +94,33 @@ public class CollisionManager {
                     }
                 }
             }
-            
-            else if (entity.getBoundingBox().intersects(playerBoundingBox)) { //Pickups
+
+            //Player intersects entity
+            else if (entity.getBoundingBox().intersects(playerBoundingBox)) {
                 
-                //Health
+                //Health Pickup
                 if (entity instanceof HealthPickup) {
                     player.heal();
                     entityIterator.remove();
                 }
 
-                //Strength                
+                //Strength Pickup           
                 else if (entity instanceof StrengthPickup) {
                     player.applyBonusDamage();
                     entityIterator.remove();
+                }
+
+                //Treadmill
+                else if (entity instanceof Treadmill) {
+                    String direction = ((Treadmill) entity).getDirection();
+                    if (direction.equals("RIGHT"))
+                        player.setX(player.getX() + ((Treadmill) entity).getMoveRate());
+                    else if (direction.equals("LEFT"))
+                        player.setX(player.getX() - ((Treadmill) entity).getMoveRate());
+                    else if (direction.equals("UP"))
+                        player.setY(player.getY() - ((Treadmill) entity).getMoveRate());
+                    else if (direction.equals("DOWN"))
+                        player.setY(player.getY() + ((Treadmill) entity).getMoveRate());
                 }
             }
         }
