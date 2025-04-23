6cd46ddf76c2a231a71767b70710db7e02072d5d
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-22 23:56:10 -0400

    Setup simple state logic

--- TheDon.java
+++ TheDon.java
@@ -1,14 +1,76 @@
 import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 public class TheDon extends Enemy {
-    private int t, atk_t;
+    private Random rand;
+    private int t, atk_t, big_t;
+    private final int CLOSE_RANGE = 20, SWITCH = 500;
+
+    private enum State {
+        PUNCH,
+        SHOOT,
+        TREAD,
+        MORTAR
+    }
+
+    private State state;
 
     public TheDon(Player player, int x, int y) {
         super(player, x, y, 80, 80, 600, -1, 4, 4, 250);
+        rand = new Random();
+        t = 0;
+        atk_t = -1;
+        big_t = 0;
     }
 
     public void update() {
-        
+        stopAttack();
+        if (state != State.TREAD && state != State.MORTAR && big_t % SWITCH == 0) {
+            if (distFromPlayer() >= CLOSE_RANGE) {
+                if (rand.nextInt() % 2 == 0)
+                    state = State.PUNCH;
+                else
+                    state = State.SHOOT;
+            } else {
+                state = State.PUNCH;
+            }
+        }
+        big_t++;
+        act(state);
+    }
+
+    private int distFromPlayer() {
+        // TODO: Calculate Euclidean distance from player
+        return -1;
+    }
+
+    private void act(State current) {
+        switch (current) {
+            case PUNCH:
+                break;
+            case SHOOT:
+                break;
+            case TREAD:
+                break;
+            case MORTAR:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void damaged(int damage) {
+        int before = health;
+        super.damaged(damage);
+        int after = health;
+        if (before > 400 && after < 400)
+            state = State.MORTAR;
+        if (before > 200 && after < 200)
+            state = State.TREAD;
+        int diff = before - after;
+        if (diff >= 100 && (state == State.MORTAR || state == State.TREAD)) {
+
+        }
     }
 
     protected Rectangle2D generateAttackBoundingBox() {
