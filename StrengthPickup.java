7e1c4111c26feddf8657116364f73f8cc3e1f082
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-24 17:13:16 -0400

    Added assets for health and strength pickups

--- StrengthPickup.java
+++ StrengthPickup.java
@@ -1,18 +1,14 @@
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-
 public class StrengthPickup extends Pickup{
+    private Animation strengthAnim;
+
     StrengthPickup(int x, int y) {
-        super(x, y);
+        super(x, y, 35, 35);
+        strengthAnim = new Animation(this, "images/strength.gif", 1, 10, 60, true);
+        drawable = strengthAnim;
+        strengthAnim.rowAnim("DEFAULT", 0);
+        strengthAnim.setState("DEFAULT");
     }
 
 
     public void update() {}
-
-
-    public void draw(Graphics2D g2) {
-        g2.setColor(Color.GRAY);
-        g2.fill(new Rectangle2D.Double(x, y, width, height));
-    }
 }
