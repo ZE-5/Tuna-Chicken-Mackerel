6eaef93ff78a1026c286ad86d1857496c488a338
ZE-5-M <zaneiedwards2003@gmail.com>
2025-04-27 02:08:50 -0400

    Added uniqueness check to Trigger class.

--- Trigger.java
+++ Trigger.java
@@ -1,14 +1,32 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 public class Trigger extends GameEntity {
     private String triggerType;
     private int triggerValue;
     private boolean isActive;
     private boolean show;
+    private static Map<String, Vector<Integer>> triggers = new HashMap<>();
 
     public Trigger(int x, int y, int width, int height, String triggerType, int triggerValue) {
         super(x, y, width, height);
+        
+        if (!triggers.containsKey(triggerType)) {
+            triggers.put(triggerType, new Vector<>());
+            triggers.get(triggerType).add(triggerValue);
+        }
+        else if (triggers.get(triggerType).contains(triggerValue)) {
+                String message = "Trigger value already exists for this trigger type. Trigger type: " + triggerType + ", Trigger value: " + triggerValue;
+                throw new IllegalArgumentException(message);
+            }
+        else
+            triggers.get(triggerType).add(triggerValue);
+        
+
+        
         this.triggerType = triggerType.toUpperCase();
         this.triggerValue = triggerValue;
         isActive = true;
